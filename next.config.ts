2bd9b92a469ef82c3328a3ecbe9ed40136a2328c
xedter20 <dextermiranda441@gmail.com>
2025-12-15 12:20:12 +0800

    fxies

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
   trailingSlash: true,
-  assetPrefix: '',
-  experimental: {
-    missingSuspenseWithCSRBailout: false
-  }
+  assetPrefix: ''
 };
 
 export default nextConfig;
